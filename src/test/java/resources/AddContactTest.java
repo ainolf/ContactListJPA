5ea3a593f292a6c1a9518bea493839450bb0c4c9
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2024-02-06 18:34:32 +0100

    Borrados comentarios.

--- src/test/java/resources/AddContactTest.java
+++ src/test/java/resources/AddContactTest.java
@@ -16,20 +16,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AddContactTest {
     private static WebDriver webDriver;
-//    private static final String pathToChromeWebdriver = "/home/oscar/Oscar/Software/chromedriver";
     private static final String localhost = "http://localhost:8080";
-//    private static final Logger log = Logger.getLogger();
-
-//    private static final Logger LOG = Logger.getLogger(ExampleResource.class);
 
     @BeforeAll
     public static void setUp() {
-//        System.setProperty("webdriver.chrome.driver", pathToChromeWebdriver);
-//        System.setProperty("webdriver.gecko.driver", pathToFirefoxDriver);
-//        webDriver = new ChromeDriver();
-//        webDriver = new FirefoxDriver();
-        // Wait before getting WebElements
-//        webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         ChromeDriverService service = new ChromeDriverService.Builder().build();
         webDriver = new ChromeDriver(service);
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
@@ -42,33 +32,27 @@ public class AddContactTest {
 
     @Given("I am in the contacts list page")
     public void i_am_in_the_contacts_list_page() {
-        // Write code here that turns the phrase above into concrete actions
         webDriver.navigate().to(localhost);
     }
     @When("II provide {string} for the name")
     public void ii_provide_for_the_name(String name) throws InterruptedException {
-//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-//        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys(name);
+        Thread.sleep(1000);
     }
     @When("I provide {string} for the surname")
     public void i_provide_for_the_surname(String surname) throws InterruptedException {
-//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//*[@id=\"surname\"]")).sendKeys(surname);
+        Thread.sleep(1000);
     }
     @When("I provide {string} for the nif")
     public void i_provide_for_the_nif(String nif) throws InterruptedException {
-//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//*[@id=\"nif\"]")).sendKeys(nif);
+        Thread.sleep(1000);
     }
     @When("I click the New button")
     public void i_click_the_new_button() throws InterruptedException {
-//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//*[@id=\"new\"]")).click();
+        Thread.sleep(1000);
     }
     @Then("The person with nif {string} is created in the agenda")
     public void the_person_with_nif_is_created_in_the_agenda(String nif) {
