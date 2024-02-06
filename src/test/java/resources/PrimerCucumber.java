a0c680355a43f781a41b8a2efba328ccf208f776
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2024-02-06 13:57:58 +0100

    Test con Quarkus.

---
+++ src/test/java/resources/PrimerCucumber.java
@@ -0,0 +1,23 @@
+package resources;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PrimerCucumber {
+    @Given("una condicion")
+    public void una_condicion() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @When("ocurre algo")
+    public void ocurre_algo() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @Then("la consecuencia")
+    public void la_consecuencia() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/features/primero.feature
@@ -0,0 +1,7 @@
+Feature: Primera característica
+  Esto debe ser un comentario.
+
+  Scenario: Texto del primer escenario
+    Given una condicion
+    When ocurre algo
+    Then la consecuencia
